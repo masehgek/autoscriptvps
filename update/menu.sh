7097827cce36ab63f0d93df2bc492a6c3a2bb76b
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-06-22 09:06:13 +0700

    x

--- update/menu.sh
+++ update/menu.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 clear
+# pewarna hidup
 m="\033[0;1;36m"
 y="\033[0;1;37m"
 yy="\033[0;1;32m"
 yl="\033[0;1;33m"
 wh="\033[0m"
+Blue='\e[1;34m'
+NC='\e[0m'
+# get info menu
+MYIP=$(curl -sS ifconfig.me)
+LOC=$(curl -sS ifconfig.co/country)
+Domen="$(cat /etc/xray/domain)"
+# TOTAL RAM
+total_ram=` grep "MemTotal: " /proc/meminfo | awk '{ print $2}'`
+totalram=$(($total_ram/1024))
+# GETTING OS INFORMATION
+source /etc/os-release
+Tipe=$NAME
+clear
+echo -e "$y-------------------------------------------------$wh"
+echo -e "  \e[1;34mPublic IP   \e[0m: $MYIP"
+echo -e "  \e[1;34mCountry     \e[0m: $LOC"
+echo -e "  \e[1;34mOS Name     \e[0m: $Tipe"
+echo -e "  \e[1;34mTotal RAM   \e[0m: ${totalram}MB"
+echo -e "  \e[1;34mDomain      \e[0m: $Domen"
+echo -e "$y-------------------------------------------------$wh"
 echo -e "$y                     MENU  $wh"
 echo -e "$y-------------------------------------------------$wh"
 echo -e "$yy 1$y.  SSH & OpenVPN MENU  $wh"
@@ -20,6 +41,8 @@ echo -e "$yy 10$y. TROJAN GFW MENU$wh"
 echo -e "$yy 11$y. TROJAN GO MENU$wh"
 echo -e "$yy 12$y. Settings$wh"
 echo -e "$yy 13$y. Exit$wh"
+echo -e "$y-------------------------------------------------$wh"
+echo -e "$yl            Telegram : t.me/givpn$wh"
 echo -e "$y-------------------------------------------------$wh"
 read -p "Select From Options [ 1 - 13 ] : " menu
 case $menu in
